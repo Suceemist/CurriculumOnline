31bb91c4400d463530233dbe495bde2bdfef8807
Ana Martínez <Behindcvp@gmail.com>
2019-02-13 11:15:09 +0100

    Add files via upload

---
+++ CurriculumVitaeOnline/js/curriculum.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+  $("#titulo_form").click(function(){
+    $("#dformacion").fadeToggle("slow");
+  });
+
+  $("#titulo_exp").click(function(){
+    $("#dexperiencia").fadeToggle("slow");
+  });
+
+  $("#titulo_idi").click(function(){
+    $("#didiomas").fadeToggle("slow");
+  });
+
+  $("#titulo_apt").click(function(){
+    $("#daptitudes").fadeToggle("slow");
+  });
+
+  $("#titulo_enl").click(function(){
+    $("#denlaces").fadeToggle("slow");
+  });
+
+
+//cambiar el estilo de css mediante jquery o javascript
+
+});
